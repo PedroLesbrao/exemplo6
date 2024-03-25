91420bd8a21125e7717bb131d62abbe6dc2df09e
Pedro Lesbrão Progamador <90758356+PedroLesbrao@users.noreply.github.com>
2024-03-25 17:29:17 -0300

    Add files via upload

---
+++ exemplo6.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <title>Document</title>
+        <link type="text/css" rel="stylesheet" href="estilo.css"/>
+    </head>
+    <body>
+        <h1>CT Desenvolvimento de Sistemas Back-End</h1>
+        <div id="container">
+            <?php
+
+            $teste = "Brasil";
+            $resultado = '<br>---$teste---<br>';
+            echo $resultado;
+            $resultado = "<br>---$teste---<br>";
+            echo $resultado;
+
+            ?>
+        </div>
+    </body>
+</html>
